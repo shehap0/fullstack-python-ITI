da7e3907d794bb6e21ba1177194f7482e5a49c91
shehap0 <shehapsherif2030@gmail.com>
2025-08-04 16:19:23 +0300

    renaming

---
+++ python/day5/day5/mods/student/day5.py
@@ -0,0 +1,13 @@
+day5 = "day5"
+def fun():
+    return "fun in day5"
+def calculations (num1  , num2):
+    # return num1+num2
+    return num1+num2 , num1-num2 , num1*num2 , num1/num2 
+
+# print (calculations(4,5))
+# summation , subtraction , mutipling , division = calculations(4,5)
+# print (summation)
+# print (subtraction)
+# print (mutipling)
+# print (division)
